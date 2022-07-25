12e1bc8774b0ff835cbb3ca30e106e9321fdd109
ranjany74 <ranjany74@gmail.com>
2022-07-25 17:43:38 -0400

    Added validator npm package.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const validator = require('validator');
+const { default: isEmail } = require('validator/lib/isEmail.js');
 const getNotes  = require('./notes.js')
 
 console.log(getNotes());
+console.log(validator.isURL('rkgmail.com'))
